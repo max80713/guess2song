789d0844cd6dbd15b96b75d9d6284bc2c909b025
Max Hsu <maxh@arisan.io>
2018-01-06 15:34:34 +0800

    Show track and options

--- imports/ui/components/hello/hello.js
+++ imports/ui/components/hello/hello.js
@@ -1,19 +1,35 @@
 import './hello.html';
+import { Random } from 'meteor/random';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  this.options = new ReactiveVar();
 });
 
 Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
   },
+  options() {
+    return Template.instance().options.get();
+  }
 });
 
 Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
+    Meteor.call('createGame', (error, result) => {
+      instance.options.set(result);
+      const track = Random.choice(result);
+      const trackId = track.id;
+      console.log(track);
+      if (instance.find('iframe')) instance.find('.track').removeChild(instance.find('iframe'));
+      var iframe = document.createElement('iframe');
+      // iframe.style.display = "none";
+      iframe.src = `https://widget.kkbox.com/v1/?id=${trackId}&type=song&terr=JP&lang=JA&autoplay=true`;
+      instance.find('.track').append(iframe);
+    });
   },
 });
