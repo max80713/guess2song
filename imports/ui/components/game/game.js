db689a89abefc7e332529028444652461ca2b087
Max Hsu <maxh@arisan.io>
2018-01-09 21:27:49 +0800

    Get playlist id from route

--- imports/ui/components/game/game.js
+++ imports/ui/components/game/game.js
@@ -1,2 +1,16 @@
 import './game.html';
 import { Random } from 'meteor/random';
+
+Template.game.onCreated(function gameOnCreated() {
+  this.tracks = new ReactiveVar([]);
+  const playlistId = FlowRouter.getParam('playlistId');
+  Meteor.call('getTracks', playlistId, (error, result) => {
+    this.tracks.get(result);
+  });
+});
+
+Template.game.helpers({
+  tracks() {
+    return Template.instance().tracks.get();
+  },
+});
