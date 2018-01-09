556579bb6604e7e305eefed7c1b98006d3f49457
Max Hsu <maxh@arisan.io>
2018-01-09 21:21:15 +0800

    Add game route

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,7 +2,8 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '../../ui/layouts/body/body.js';
-import '../../ui/pages/home/home.js';
+import '../../ui/components/hello/hello.js';
+import '../../ui/components/game/game.js';
 import '../../ui/pages/not-found/not-found.js';
 
 BlazeLayout.setRoot('body');
@@ -10,7 +11,14 @@ BlazeLayout.setRoot('body');
 FlowRouter.route('/', {
   name: 'App.home',
   action() {
-    BlazeLayout.render('App_body', { main: 'App_home' });
+    BlazeLayout.render('App_body', { main: 'hello' });
+  },
+});
+
+FlowRouter.route('/:playlistId', {
+  name: 'App.home',
+  action() {
+    BlazeLayout.render('App_body', { main: 'game' });
   },
 });
 
