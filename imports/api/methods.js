b63df1e010859508b344652af0fab6218d456854
Max Hsu <maxh@arisan.io>
2018-01-12 11:28:35 +0800

    Add method for retrieving fb user picture

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -1,4 +1,7 @@
 import { Meteor } from 'meteor/meteor';
+import { Promise } from 'meteor/promise';
+
+import FB from 'fb';
 
 import { Games } from './games/games.js';
 import { Playlists } from './playlists/playlists.js';
@@ -10,5 +13,21 @@ Meteor.methods({
   },
   'getTracks'(playlistId) {
     return Tracks.find({ playlist_id: playlistId }).fetch();
+  },
+  'getPictureUrl'() {
+    const user = Meteor.user();
+    if (!user) return '';
+    const fbAccessToken = user.services.facebook.accessToken;
+    FB.setAccessToken(fbAccessToken);
+    const fbAPI = () => new Promise((resolve, reject) => {
+      FB.api('/me?fields=picture', 'get', (res) => {
+        if(!res || res.error) {
+          reject(!res ? 'error occurred' : res.error);
+          return;
+        }
+        resolve(res.picture.data.url);
+      });
+    });
+    return Promise.await(fbAPI());
   },
 });
