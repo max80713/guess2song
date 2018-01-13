9f39b57234921a6af5e5c2ddcc3935077b77908c
Max Hsu <maxh@arisan.io>
2018-01-14 02:54:55 +0800

    Add loader

--- imports/ui/components/hello/hello.js
+++ imports/ui/components/hello/hello.js
@@ -1,10 +1,11 @@
 import './hello.html';
+import '../loader/loader.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   this.playlists = new ReactiveVar([]);
   Meteor.call('getPlaylists', (error, result) => {
     this.playlists.set(result);
-  })
+  });
 });
 
 Template.hello.helpers({
