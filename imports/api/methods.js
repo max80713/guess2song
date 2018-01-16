10bd4a2829004d9f895f02ceb45a2e3907b5288b
Max Hsu <maxh@arisan.io>
2018-01-16 23:39:51 +0800

    Remove local cache of kkbox api content

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -1,37 +1,48 @@
 import { Meteor } from 'meteor/meteor';
 import { Promise } from 'meteor/promise';
 
+import { Api } from '@kkbox/kkbox-js-sdk';
 import FB from 'fb';
 
 import { Games } from './games/games.js';
+import { Tokens } from './tokens/tokens.js';
 import { Playlists } from './playlists/playlists.js';
 import { Tracks } from './tracks/tracks.js';
 
+const fetchCharts = (api) => new Promise(resolve => api.chartFetcher.fetchCharts().then(response => resolve(response.data.data)));
+const fetchTracks = (api, playlistId) => new Promise(resolve => api.chartFetcher.setPlaylistID(playlistId).fetchTracks().then(response => resolve(response.data.data)));
+
+// playlists.forEach((playlist) => {
+//   if (!playlist.champion_id) return;
+//   const champion = Meteor.users.findOne(playlist.champion_id);
+//   if (!champion) return;
+//   playlist.champion_name = champion.profile.name;
+//   const fbAccessToken = champion.services.facebook.accessToken;
+//   FB.setAccessToken(fbAccessToken);
+//   const fbAPI = () => new Promise((resolve, reject) => {
+//     FB.api('/me?fields=picture', 'get', (res) => {
+//       if(!res || res.error) {
+//         reject(!res ? 'error occurred' : res.error);
+//         return;
+//       }
+//       resolve(res.picture.data.url);
+//     });
+//   });
+//   playlist.champion_picture = Promise.await(fbAPI())
+// });
+
 Meteor.methods({
   'getPlaylists'() {
-    const playlists = Playlists.find().fetch();
-    playlists.forEach((playlist) => {
-      if (!playlist.champion_id) return;
-      const champion = Meteor.users.findOne(playlist.champion_id);
-      if (!champion) return;
-      playlist.champion_name = champion.profile.name;
-      const fbAccessToken = champion.services.facebook.accessToken;
-      FB.setAccessToken(fbAccessToken);
-      const fbAPI = () => new Promise((resolve, reject) => {
-        FB.api('/me?fields=picture', 'get', (res) => {
-          if(!res || res.error) {
-            reject(!res ? 'error occurred' : res.error);
-            return;
-          }
-          resolve(res.picture.data.url);
-        });
-      });
-      playlist.champion_picture = Promise.await(fbAPI())
-    });
+    const token = Tokens.findOne();
+    const api = new Api(token.access_token);
+    const playlists = Promise.await(fetchCharts(api));
     return playlists;
   },
   'getTracks'(playlistId) {
-    return Tracks.find({ playlist_id: playlistId }).fetch();
+    const token = Tokens.findOne();
+    const api = new Api(token.access_token);
+    const tracks = Promise.await(fetchTracks(api, playlistId));
+    return tracks;
   },
   'getPictureUrl'() {
     const user = Meteor.user();
